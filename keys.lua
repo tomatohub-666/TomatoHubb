2424f4b7ed5a671de99f1f57b3d26e28a13e2916
tomatohub-666 <hoangpham.03112010@gmail.com>
2026-01-01 20:11:21 +0700

    keys.lua

---
+++ keys.lua
@@ -0,0 +1,11 @@
+return {
+    -- [ADMIN]
+    ["TMH_ADM_v21_99x88_#$!_VinhVien_@2026"] = {Type = "Admin", Expire = "9999-12-31"}, -- Không bao giờ hết hạn
+    
+    -- [PREMIUM - GIA HẠN THEO NGÀY]
+    ["TMH_PRM_88x22_Vip_#771_Exp:2026-02-01"] = {Type = "Premium", Expire = "2026-01-02"}, -- Hết hạn 01/02/2026
+    ["TMH_PRM_11x99_Gold_#002_Exp:2026-03-15"] = {Type = "Premium", Expire = "2026-03-15"}, -- Hết hạn 15/03/2026
+    
+    -- [FREE - KEY NGÀY]
+    ["TMH_FREE_v2.0_Daily_Exp:2026-01-05"] = {Type = "Free", Expire = "2026-01-05"} -- Hết hạn 05/01/2026
+}
