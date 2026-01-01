234050b57b7d820c14aeed314ec71fa587b45f28
tomatohub-666 <hoangpham.03112010@gmail.com>
2026-01-01 22:13:34 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,2 @@
-_G.Key = "NHẬP_KEY_CỦA_BẠN"
--- Sử dụng pcall để tránh văng game nếu link lỗi
-pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/tomatohub-666/TomatoHubb/main/Main.lua", true))()
-end)
+_G.Key = "DÁN_KEY_CỦA_BẠN_VÀO_ĐÂY"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tomatohub-666/TomatoHubb/main/loader.lua"))()
