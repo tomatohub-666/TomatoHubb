0f616cbbcabe32bef040085a384319c2ada1c739
tomatohub-666 <hoangpham.03112010@gmail.com>
2026-01-02 19:32:06 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/tomatohub-666/TomatoHubb/main/loader.lua?v=" .. tostring(os.time())))()
+print("--- KIEU TRA MAIN.LUA ---")
+local ScreenGui = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
+ScreenGui.Parent = game.CoreGui
+TextLabel.Parent = ScreenGui
+TextLabel.Size = UDim2.new(0, 200, 0, 50)
+TextLabel.Position = UDim2.new(0.5, -100, 0.5, -25)
+TextLabel.Text = "MAIN LUA DA CHAY!"
 print("CHUC MUNG! TOMATO HUB DA CHAY!")
 -- Dán code hack của bạn dưới đây
