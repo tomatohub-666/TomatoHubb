98c1cad1c3598caf199aa915706bfd2ad88d00dc
tomatohub-666 <hoangpham.03112010@gmail.com>
2026-01-02 19:24:25 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
-_G.Key = "KEY_CỦA_BẠN"
--- Thêm dấu hỏi chấm và số ngẫu nhiên để xóa cache
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tomatohub-666/TomatoHubb/main/loader.lua?v=" .. math.random(1,1000)))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tomatohub-666/TomatoHubb/main/loader.lua?v=" .. tostring(os.time())))()
+print("CHUC MUNG! TOMATO HUB DA CHAY!")
+-- Dán code hack của bạn dưới đây
