f25233e77c4c598d10befc232f8ee6ee13f2a028
tomatohub-666 <hoangpham.03112010@gmail.com>
2026-01-02 19:10:25 +0700

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,8 +1,7 @@
 return {
-    -- [ADMIN] Để số thật lớn để không bao giờ hết hạn
+    -- [ADMIN]
     ["TMH_ADM_v21_99x88_#$!_VinhVien_@2026"] = {Type = "Admin", Expire = 9999999999},
     
-    -- [FREE KEY] Sử dụng os.time() + số giây (ví dụ 1 ngày là 86400 giây)
-    -- Để test, bạn hãy để số 9999999999 cho tất cả để đảm bảo không bị Kick do hết hạn
-    ["TMH_FREE_v2.0_Daily_Exp:2026-01-05"] = {Type = "Free", Expire = 9999999999}
+    -- [FREE KEY] - Thêm dấu ngoặc vuông và ngoặc kép chuẩn xác
+    ["TMH_FREE_v2.0_Daily_Exp:2026-01-05"] = {Type = "Free", Expire = 9999999999},
 }
