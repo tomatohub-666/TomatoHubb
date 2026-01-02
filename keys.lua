ac4433c7fa98569ff2a1984f52688e65c2cbb3af
tomatohub-666 <hoangpham.03112010@gmail.com>
2026-01-02 19:16:50 +0700

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,7 +1,4 @@
 return {
-    -- [ADMIN]
     ["TMH_ADM_v21_99x88_#$!_VinhVien_@2026"] = {Type = "Admin", Expire = 9999999999},
-    
-    -- [FREE KEY] - Thêm dấu ngoặc vuông và ngoặc kép chuẩn xác
-    ["TMH_FREE_v2.0_Daily_Exp:2026-01-05"] = {Type = "Free", Expire = 9999999999},
+    ["TMH_FREE_v2.0_Daily_Exp:2026-01-05"] = {Type = "Free", Expire = 9999999999}, -- Lưu ý dấu phẩy ở cuối dòng
 }
